d6d5610170af0a2a38c4e65af9a00b0dcae9ffc7
MUKESH KUMAR PATEL <kumarmukeshpatel57@gmail.com>
2021-03-26 15:22:56 +0530

    some practice basic

---
+++ src/com/mukesh/introduction_to_java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mukesh.introduction_to_java;
+
+import com.mukesh.typeConversion.TypeConvirson;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to Java Programming!");
+
+//        OtherOperators obj = new OtherOperators();
+
+        TypeConvirson typeConvirson = new TypeConvirson();
+    }
+}
