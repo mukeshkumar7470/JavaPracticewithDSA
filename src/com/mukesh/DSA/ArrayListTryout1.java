de05dfcd646a6fe3c2c8faee9ca1c0391315a99a
MUKESH KUMAR PATEL <kumarmukeshpatel57@gmail.com>
2021-04-05 15:59:52 +0530

    ArrayList

---
+++ src/com/mukesh/DSA/ArrayListTryout1.java
@@ -0,0 +1,16 @@
+package com.mukesh.DSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTryout1 {
+    public static void main(String[] args) {
+        List<String> food = new ArrayList<String>(); // Creating a list of String elements
+        food.add("Pizza"); // Adding elements
+        food.add("Burger");
+        food.add("Pasta");
+        food.add("Sandwich");
+        food.add("Samosa");
+        System.out.println("Food items: " + food);
+    }
+}
