8409950fbb81b95fff4e4d2b315ef92a05910943
Sumit Ojha <sumitojha987@gmail.com>
2021-07-28 02:08:28 +0530

    CodingNinja

---
+++ src/com/mukesh/CodingNinja/assignment/AllFibonacciNumbers.java
@@ -0,0 +1,25 @@
+package com.mukesh.CodingNinja.assignment;
+
+public class AllFibonacciNumbers {
+    // Driver Code
+    public static void main(String[] args)
+    {
+        printFibonacciNumbers(7);
+    }
+
+    private static void printFibonacciNumbers(int n) {
+
+        int f1 = 0, f2 = 1, i;
+
+        if (n < 1)
+            return;
+        System.out.print(f1 + " ");
+        for (i = 1; i < n; i++)
+        {
+            System.out.print(f2 + " ");
+            int next = f1 + f2;
+            f1 = f2;
+            f2 = next;
+        }
+    }
+}
